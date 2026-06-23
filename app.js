4ad8c7d24d03794f06af537bf64b3460dc51ad68
Wahid Fayeq <wahid.fayaq@yahoo.com>
2026-06-23 11:05:52 -0400

    Connect backend to MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const PORT = 3001;
+
+const { PORT = 3001, MONGODB_URI = "mongodb://127.0.0.1:27017/news-explorer" } =
+  process.env;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+
+    process.exit(1);
+  });
 
 app.get("/", (req, res) => {
   res.send({ message: "News Explorer API is running" });
