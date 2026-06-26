3ab7877019719bfe3e7fdc8b9a1af8585f10aa26
Wahid Fayeq <wahid.fayaq@yahoo.com>
2026-06-26 13:16:15 -0400

    New updates

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,13 @@
-const { JWT_SECRET = "dev-secret" } = process.env;
+require("dotenv").config();
+
+const {
+  PORT = 3001,
+  MONGODB_URI = "mongodb://127.0.0.1:27017/news-explorer",
+  JWT_SECRET = "dev-secret",
+} = process.env;
 
 module.exports = {
+  PORT,
+  MONGODB_URI,
   JWT_SECRET,
 };
